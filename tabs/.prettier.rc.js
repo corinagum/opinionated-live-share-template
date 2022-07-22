6613b7a93342a9aad5f9fcd8e7be13bc89421920
Corina Gum <>
2022-07-21 22:41:30 -0700

    More prettier / README

---
+++ tabs/.prettier.rc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: "avoid",
+  bracketSameLine: "false",
+  bracketSpacing: true,
+  endOfLine: "crlf",
+  htmlWhitespaceSensitivity: "strict",
+  printWidth: 120,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5"
+};
